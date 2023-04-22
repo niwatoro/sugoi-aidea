5f9807e0384dba2e903d9f87848590df709cc9c8
niwatoro <girlwithskyhairpin@gmail.com>
2023-04-23 02:35:08 +0900

    make the layout of mypage

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,12 +1,13 @@
+import { classNames } from "@/lib/class-names";
 import { FC } from "react";
 
-type Props = { src: string };
+type Props = { src: string; expands?: boolean };
 
-const Avatar: FC<Props> = ({ src }) => {
+const Avatar: FC<Props> = ({ src, expands }) => {
   if (src) {
-    return <img src={src} className="w-10 h-10 rounded-full" />;
+    return <img src={src} className={classNames(expands ?? false ? "w-full h-full" : "w-10 h-10", "rounded-full")} />;
   } else {
-    return <div className="w-10 h-10 rounded-full bg-gray-300" />;
+    return <div className={classNames(expands ?? false ? "w-full h-full" : "w-10 h-10", "rounded-full", "bg-gray-300")} />;
   }
 };
 
--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,8 +1,25 @@
+import Avatar from "@/components/avatar";
 import UserGuard from "@/guards/user-guard";
 import { NextPage } from "next";
 
 const MyPage: NextPage = () => {
-  return <UserGuard>{(user) => <div>{user.name}</div>}</UserGuard>;
+  return (
+    <UserGuard>
+      {(user) => (
+        <div className="w-screen flex justify-center mt-10">
+          <div className="w-[800px] flex gap-x-10">
+            <div className="w-36 h-36">
+              <Avatar src={user.photoUrl} expands />
+            </div>
+            <div>
+              <div className="font-bold text-2xl">{user.name}</div>
+              <div>{user.bio}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </UserGuard>
+  );
 };
 
 export default MyPage;
