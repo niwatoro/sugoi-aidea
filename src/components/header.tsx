eaf1d57b8f60c2ca8120ffa30015d13750de3f92
niwatoro <girlwithskyhairpin@gmail.com>
2023-04-22 21:49:23 +0900

    implement header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,26 @@
+import { useAuth } from "@/context/auth";
+import { login } from "@/lib/auth";
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 const Header: FC = () => {
+  const user = useAuth();
+  const [isWaiting, setIsWaiting] = useState(false);
+
+  const signIn = () => {
+    setIsWaiting(true);
+
+    login()
+      .catch((error) => console.error(error?.message))
+      .finally(() => setIsWaiting(false));
+  };
+
   return (
-    <header>
-      <h1>
-        <Link href={"/"}>
-          <div className="font-logo">SUGOI-AIDEA</div>
-        </Link>
-      </h1>
+    <header className="h-16 flex items-center justify-between px-8">
+      <Link href={"/"}>
+        <div className="font-logo text-xl">SugoiAidea</div>
+      </Link>
+      <div>{user === null ? !isWaiting ? <button onClick={signIn}>ログイン</button> : null : <button>ユーザーメニュー</button>}</div>
     </header>
   );
 };
