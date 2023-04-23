11600992c09c12d901d6ce63e7bd3c20b2224178
niwatoro <girlwithskyhairpin@gmail.com>
2023-04-23 11:32:45 +0900

    add textareafield

---
+++ src/components/field/textarea-field.tsx
@@ -0,0 +1,23 @@
+import { classNames } from "@/lib/class-names";
+import { ReactNode } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+import TextareaAutosize, { TextareaAutosizeProps } from "react-textarea-autosize";
+import FieldGroup from "./field-group";
+
+type Props = {
+  label: string;
+  error?: string;
+  currentlength?: number;
+  action: ReactNode;
+  register: UseFormRegisterReturn;
+} & TextareaAutosizeProps;
+
+const TextareaField = ({ label, error, currentlength, action, register, className, ...props }: Props) => {
+  return (
+    <FieldGroup label={label} error={error} currentlength={currentlength} action={action} maxLength={props.maxLength} required={props.required} id={register.name}>
+      <TextareaAutosize id={register.name} className={classNames("flex-1 border rounded px-2 py-1 w-full", className)}></TextareaAutosize>
+    </FieldGroup>
+  );
+};
+
+export default TextareaField;
